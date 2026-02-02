82a572eedcf447acdd0ae37da9acdcade5581d0b
glbequetteBU <xboxminesplat@gmail.com>
2026-02-02 00:27:42 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,12 @@
 function toggleForms() {
+    console.log("Toggle function clicked!"); // This helps you debug
     const loginForm = document.getElementById('login-form');
     const signupForm = document.getElementById('signup-form');
     
     if (loginForm && signupForm) {
         loginForm.classList.toggle('hidden');
         signupForm.classList.toggle('hidden');
+    } else {
+        console.error("Form elements not found!");
     }
 }
